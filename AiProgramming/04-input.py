53784921a18df36792d04a7571fcf9ea9125254d
fjadidi2001 <jadidi7300@gmail.com>
2023-12-05 00:10:43 +0330

    create 04 program

---
+++ AiProgramming/04-input.py
@@ -0,0 +1,8 @@
+
+print('Your name:')
+name = input()
+print('Youur age:')
+age = int(input())
+print('Your marks:')
+marks = float(input())
+print(f'My name is {name} ,Im {age} years old, my marks is {marks}')
