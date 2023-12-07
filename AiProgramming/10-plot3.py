d96758fbd665e75e5ebb153653d7eb325d19852b
fjadidi2001 <jadidi7300@gmail.com>
2023-12-07 21:09:21 +0330

    fx some issue in 10.py

--- AiProgramming/10-plot3.py
+++ AiProgramming/10-plot3.py
@@ -12,10 +12,10 @@ def func2(x):
 def func3(x):
     return x**3 + 9
 
-x = np.linspace(-10, 10, 50)
+x = np.linspace(-10, 10, 10)
 
 plt.plot(x, func1(x), color = 'blue', marker = "s", label='3x+4')
 plt.plot(x, func2(x), color = 'red', marker = "o", label='2x2+1')
-plt.plot(x, func3(x), color = 'green', marker = "d", label='x3+9')
+plt.plot(x, func3(x), color = 'green', marker = "p", label='x3+9')
 plt.legend()
 plt.show()
