c8cd03debf7c8a0ca22cf33a3d342e6ce52efa1b
fjadidi2001 <fjadidi2001@gmail.com>
2024-05-03 23:06:44 +0330

    update norm.py

--- unversity-learning/data-mining/norm.py
+++ unversity-learning/data-mining/norm.py
@@ -1,8 +1,17 @@
+import numpy as np
+# create u
 u = np.array([1,6])
-u
+print('u is',u)
+# create v
 v = np.array([4,2])
-v
-u+v
-np.linalg.norm(u+v)
-np.linalg.norm(u)+np.linalg.norm(v)
+print('v is',v)
+print('sum u+v')
+print(u+v)
+'''
+"linalg" is an abbreviation for "linear algebra,"
+which is a branch of mathematics that deals with vectors, vector spaces, linear transformations, and systems of linear equations.
+It is commonly used in various fields such as physics, engineering, computer science, and economics.
+'''
+print(np.linalg.norm(u+v))
+print(np.linalg.norm(u)+np.linalg.norm(v))
 
